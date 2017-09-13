df19885e153c5d9766f8e7502ad4a12e5e67a7c4
zhaoxinfu <zhaoxinfu@zhaoxinfudeMac-mini.local>
2017-09-13 20:58:35 +0800

    加一个bean

--- app/src/main/java/com/study/b8a3/kotlin/activity/MainActivity.kt
+++ app/src/main/java/com/study/b8a3/kotlin/activity/MainActivity.kt
@@ -7,12 +7,14 @@ import android.widget.Button
 import android.widget.TextView
 import com.study.b8a3.kotlin.R
 import com.study.b8a3.kotlin.base.BaseActivity
+import com.study.b8a3.kotlin.bean.Person
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity(), View.OnClickListener {
 
     private var mFirstTV: TextView? = null
     private var mDataRev: RecyclerView? = null
+    private var mPerson = Person("Jim", 20, "www.url")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +25,7 @@ class MainActivity : BaseActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        toast(mBtnTest.text)
+        toast("${mBtnTest.text} ${mPerson.name}")
         SLog(mBtnTest.text)
         SLog(mBtnTest.text, "TEST")
         SLog(mBtnTest.text, "TEST", 4)
---
+++ app/src/main/java/com/study/b8a3/kotlin/bean/Bean.kt
@@ -0,0 +1,6 @@
+package com.study.b8a3.kotlin.bean
+
+/**
+ * Created by zhaoxinfu on 2017/9/13.
+ */
+data class Person(var name:String, var age:Int, var imgUrl:String)
